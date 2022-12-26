7aae97a695511ad9735e38f25760b05fc2ee9391
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-12-26 18:09:12 +0545

    chores: deps

--- src/language.loader.ts
+++ src/language.loader.ts
@@ -1,17 +1,12 @@
-import { readdirSync } from 'fs';
-import { fileURLToPath } from 'node:url';
+import fs from 'fs';
 import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 const words: Record<string, string[]> = {
   all: [],
 };
 
 /* Reading the files in the data directory and then adding them to the words object. */
-readdirSync(__dirname + '/data').forEach((file: string) => {
+fs.readdirSync(__dirname + '/data').forEach((file: string) => {
   const list = require(path.join(__dirname, 'data', file));
   words[file.slice(0, -5)] = list;
   words.all = Array.prototype.concat.apply(words.all, list);
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,78 +1,77 @@
 import Profanease from '../src/index';
 import * as wordList from '../src/language.loader';
-import { describe, expect, test } from 'vitest';
 
+describe('Post', () => {
+  it('Profanease: Default', () => {
+    let isProfane = new Profanease({ lang: 'en' });
+    expect(isProfane.censor('I am fucking asshole')).toEqual('I am ******* *******');
+  });
 
-test('Profanease: Default', () => {
-  let isProfane = new Profanease({ lang: 'en' });
-  expect(isProfane.censor('I am fucking asshole')).toEqual('I am ******* *******');
-});
+  it('Profanease:Clean lang :en', () => {
+    let isProfane = new Profanease({ lang: 'en' });
+    expect(isProfane.censor("Don't be an ash0le")).toEqual("Don't be an ******");
+  });
 
-test('Profanease:Clean lang :en', () => {
-  let isProfane = new Profanease({ lang: 'en' });
-  expect(isProfane.censor("Don't be an ash0le")).toEqual("Don't be an ******");
-});
+  it('Profanease:Check lang :en', () => {
+    let isProfane = new Profanease({ lang: 'en' });
+    expect(isProfane.check("Don't be an ash0le")).toBe(true);
+  });
 
-test('Profanease:Check lang :en', () => {
-  let isProfane = new Profanease({ lang: 'en' });
-  expect(isProfane.check("Don't be an ash0le")).toBe(true);
-});
+  it('Profanease: placeHolder :x', () => {
+    let isProfane = new Profanease({ placeHolder: 'x' });
+    expect(isProfane.censor("Don't be an ash0le")).toEqual("Don't be an xxxxxx");
+  });
 
+  it('Profanease: addWords', () => {
+    let isProfane = new Profanease();
+    isProfane.addWords(['some', 'bad', 'word']);
+    expect(isProfane.censor('some bad word!')).toBe('**** *** ****!');
+  });
 
-test('Profanease: placeHolder :x', () => {
-  let isProfane = new Profanease({ placeHolder: 'x' });
-  expect(isProfane.censor("Don't be an ash0le")).toEqual("Don't be an xxxxxx");
-});
+  it('Profanease: exclude initialize then same addWords', () => {
+    let isProfane = new Profanease({ exclude: ['ash0le', 'some', 'bad', 'word'] });
+    isProfane.addWords(['some', 'bad', 'word']);
+    expect(isProfane.censor('some bad word like ash0le!')).toBe('**** *** **** like ash0le!');
+  });
 
-test('Profanease: addWords', () => {
-  let isProfane = new Profanease();
-  isProfane.addWords(['some', 'bad', 'word']);
-  expect(isProfane.censor('some bad word!')).toBe('**** *** ****!');
-});
+  it('Profanease:Clean addWords on initialize', () => {
+    let isProfane = new Profanease({ list: ['some', 'bad', 'word'] });
+    expect(isProfane.censor('some bad word!')).toEqual('**** *** ****!');
+  });
 
-test('Profanease: exclude initialize then same addWords', () => {
-  let isProfane = new Profanease({ exclude: ['ash0le', 'some', 'bad', 'word'] });
-  isProfane.addWords(['some', 'bad', 'word']);
-  expect(isProfane.censor('some bad word like ash0le!')).toBe('**** *** **** like ash0le!');
-});
+  it('Profanease:Check addWords on initialize', () => {
+    let isProfane = new Profanease({ list: ['some', 'bad', 'word'] });
+    expect(isProfane.check('some bad word!')).toBe(true);
+  });
 
-test('Profanease:Clean addWords on initialize', () => {
-  let isProfane = new Profanease({ list: ['some', 'bad', 'word'] });
-  expect(isProfane.censor('some bad word!')).toEqual('**** *** ****!');
-});
+  it('Profanease:Clean emptyList:True', () => {
+    let isProfane = new Profanease({ emptyList: true });
+    expect(isProfane.censor('hell this wont censor anything')).toEqual('hell this wont censor anything');
+  });
 
-test('Profanease:Check addWords on initialize', () => {
-  let isProfane = new Profanease({ list: ['some', 'bad', 'word'] });
-  expect(isProfane.check('some bad word!')).toBe(true);
-});
+  it('Profanease:Check emptyList:True', () => {
+    let isProfane = new Profanease({ emptyList: true });
+    expect(isProfane.check('hell this wont censor anything')).toBe(false);
+  });
 
-test('Profanease:Clean emptyList:True', () => {
-  let isProfane = new Profanease({ emptyList: true });
-  expect(isProfane.censor('hell this wont censor anything')).toEqual('hell this wont censor anything');
-});
+  it('Profanease: emptyList:True and List of array passing', () => {
+    let isProfane = new Profanease({ emptyList: true, list: ['some', 'bad', 'word'] });
+    expect(isProfane.censor('hell this wont censor anything')).toEqual('hell this wont censor anything');
+  });
 
-test('Profanease:Check emptyList:True', () => {
-  let isProfane = new Profanease({ emptyList: true });
-  expect(isProfane.check('hell this wont censor anything')).toBe(false);
-});
+  it('Profanease: removeWords', () => {
+    let isProfane = new Profanease();
+    isProfane.removeWords(['hells', 'sadist']);
+    expect(isProfane.censor('some hells word!')).toEqual('some hells word!');
+  });
 
-test('Profanease: emptyList:True and List of array passing', () => {
-  let isProfane = new Profanease({ emptyList: true, list: ['some', 'bad', 'word'] });
-  expect(isProfane.censor('hell this wont censor anything')).toEqual('hell this wont censor anything');
-});
+  it('Profanease: wordsList for es', () => {
+    let isProfane = new Profanease();
+    expect(isProfane.wordsList('es')).toEqual(expect.arrayContaining(wordList.default.es));
+  });
 
-test('Profanease: removeWords', () => {
-  let isProfane = new Profanease();
-  isProfane.removeWords(['hells', 'sadist']);
-  expect(isProfane.censor('some hells word!')).toEqual('some hells word!');
-});
-
-test('Profanease: wordsList for es', () => {
-  let isProfane = new Profanease();
-  expect(isProfane.wordsList('es')).toEqual(expect.arrayContaining(wordList.default.es));
-});
-
-test('Profanease: wordsList for en', () => {
-  let isProfane = new Profanease();
-  expect(isProfane.wordsList()).toEqual(expect.arrayContaining(wordList.default.en));
+  it('Profanease: wordsList for en', () => {
+    let isProfane = new Profanease();
+    expect(isProfane.wordsList()).toEqual(expect.arrayContaining(wordList.default.en));
+  });
 });

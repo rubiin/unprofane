75be4ad1dc5ff1d024be4536fee9e448adb0e745
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-01-21 18:39:33 +0545

    chores: md files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import * as wordList from './language.loader';
+import * as wordList from './language.loader'
 
 /* An interface that is used to define the type of the options that are passed to the constructor. */
 interface UnprofaneOptions {
-  lang?: string;
-  placeHolder?: string;
-  exclude?: string[];
-  list?: string[];
-  emptyList?: boolean;
-  regex?: RegExp;
-  replaceRegex?: RegExp;
+  lang?: string
+  placeHolder?: string
+  exclude?: string[]
+  list?: string[]
+  emptyList?: boolean
+  regex?: RegExp
+  replaceRegex?: RegExp
 }
 
 export default class Unprofane {
-  private lang!: string;
-  private list!: string[];
-  private placeHolder!: string;
-  private regex!: RegExp;
-  private replaceRegex!: RegExp;
-  private exclude!: string[];
+  private lang!: string
+  private list!: string[]
+  private placeHolder!: string
+  private regex!: RegExp
+  private replaceRegex!: RegExp
+  private exclude!: string[]
 
   constructor(option?: UnprofaneOptions) {
     option = {
@@ -27,12 +27,12 @@ export default class Unprofane {
       replaceRegex: /\w/g,
       exclude: [],
       ...option,
-    };
-    Object.assign(this, option);
-    this.list =
-      option && option.emptyList
+    }
+    Object.assign(this, option)
+    this.list
+      = option && option.emptyList
         ? []
-        : Array.prototype.concat.apply(wordList.default[this.lang], option && option.list ? option.list : []);
+        : Array.prototype.concat.apply(wordList.default[this.lang], option && option.list ? option.list : [])
   }
 
   /**
@@ -46,9 +46,9 @@ export default class Unprofane {
     return wordToClean
       .split(/\b/)
       .map((word) => {
-        return this.check(word) ? this.replaceWord(word) : word;
+        return this.check(word) ? this.replaceWord(word) : word
       })
-      .join('');
+      .join('')
   }
 
   /**
@@ -59,10 +59,10 @@ export default class Unprofane {
   check(wordToCheck: string): boolean {
     return (
       this.list.filter((word) => {
-        const wordExp = new RegExp(`\\b${word.replace(/(\W)/g, '\\$1')}\\b`, 'gi');
-        return this.exclude.indexOf(word.toLowerCase()) >= 0 ? false : true && wordExp.test(wordToCheck);
+        const wordExp = new RegExp(`\\b${word.replace(/(\W)/g, '\\$1')}\\b`, 'gi')
+        return this.exclude.includes(word.toLowerCase()) ? false : true && wordExp.test(wordToCheck)
       }).length > 0 || false
-    );
+    )
   }
 
   /**
@@ -73,7 +73,7 @@ export default class Unprofane {
    * list.
    */
   wordsList(lang?: string): string[] {
-    return lang && this.isValidLang(lang) ? wordList.default[lang] : this.list;
+    return lang && this.isValidLang(lang) ? wordList.default[lang] : this.list
   }
 
   /**
@@ -82,22 +82,21 @@ export default class Unprofane {
    * @param {string[]} words - string[]
    */
   addWords(words: string[]): void {
-    this.list.push(...words);
+    this.list.push(...words)
     words
-      .map((word) => word.toLowerCase())
+      .map(word => word.toLowerCase())
       .forEach((word) => {
-        if (this.exclude.indexOf(word) >= 0) {
-          this.exclude.splice(this.exclude.indexOf(word), 1);
-        }
-      });
+        if (this.exclude.includes(word))
+          this.exclude.splice(this.exclude.indexOf(word), 1)
+      })
   }
 
   /* Taking an array of words and pushing them to the exclude array. */
   removeWords(words: string[]): void {
     words.map((word) => {
-      word.toLowerCase();
-      this.exclude.push(word);
-    });
+      word.toLowerCase()
+      this.exclude.push(word)
+    })
   }
 
   /**
@@ -108,13 +107,11 @@ export default class Unprofane {
    * Otherwise -1 is returned.
    */
   private isValidLang(lang: string): boolean {
-    return Object.keys(wordList.default)
+    return !!Object.keys(wordList.default)
       .map((key) => {
-        return key === lang;
+        return key === lang
       })
-      .indexOf(true) >= 0
-      ? true
-      : false;
+      .includes(true)
   }
 
   /**
@@ -125,6 +122,6 @@ export default class Unprofane {
    * placeHolder.
    */
   private replaceWord(wordToReplace: string): string {
-    return wordToReplace.replace(this.regex, '').replace(this.replaceRegex, this.placeHolder);
+    return wordToReplace.replace(this.regex, '').replace(this.replaceRegex, this.placeHolder)
   }
 }
--- src/language.loader.ts
+++ src/language.loader.ts
@@ -1,15 +1,15 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 const words: Record<string, string[]> = {
   all: [],
-};
+}
 
 /* Reading the files in the data directory and then adding them to the words object. */
-fs.readdirSync(__dirname + '/data').forEach((file: string) => {
-  const list = require(path.join(__dirname, 'data', file));
-  words[file.slice(0, -5)] = list;
-  words.all = Array.prototype.concat.apply(words.all, list);
-});
+fs.readdirSync(path.join(__dirname,'/data')).forEach((file: string) => {
+  const list = require(path.join(__dirname, 'data', file))
+  words[file.slice(0, -5)] = list
+  words.all = Array.prototype.concat.apply(words.all, list)
+})
 
-export default words;
+export default words
